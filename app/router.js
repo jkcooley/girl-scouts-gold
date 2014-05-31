6bd40243877f10f3d056afe53859065ac6876819
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2014-05-31 16:33:02 -0500

    Added messages for admins

--- app/router.js
+++ app/router.js
@@ -4,12 +4,18 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('admin', function(){
+    //Projects
     this.route('projects');
     this.route('project', {path: '/projects/:post_id'});
-
+    //Users
     this.route('users');
     this.route('user', {path: '/users/:user_id'});
+    //Messages
+    this.resource('messages', function(){
+    	this.route('message', {path: ':message_id'});
+    });
   });
+
 
   this.resource('user', function(){
     
--- app/routes/messages/message.js
+++ app/routes/messages/message.js
@@ -1,5 +1,5 @@
 export default Ember.Route.extend({
-  model: function(){
-    return [];
-  }
+	model: function(){
+		return {};
+	}
 });
