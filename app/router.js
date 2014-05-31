e032bcb471dd8e98684e07e628dde659942e3563
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2014-05-31 14:55:41 -0500

    Added admin routes for user management

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ Router.map(function() {
   this.resource('admin', function(){
     this.route('projects');
     this.route('project', {path: '/projects/:post_id'});
+
+    this.route('users');
+    this.route('user', {path: '/users/:user_id'});
   });
 
   this.resource('user', function(){
